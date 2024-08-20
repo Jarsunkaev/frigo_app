aa1f7c25d179a79fd6da50765777aa62df115492
Juszuf Arsunkaev <juszuf_1@icloud.com>
2024-08-21 01:02:41 +0200

    minor fixes

--- pages/generate.tsx
+++ pages/generate.tsx
@@ -9,21 +9,6 @@ function HomePage() {
   return (
     <AuthProvider>
       <div className="bg-gradient-to-b from-amber-50 to-amber-100 min-h-screen font-['DM_Sans']">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         <div className="fixed inset-0 z-0 pointer-events-none overflow-hidden">
           <svg className="absolute top-0 left-0 w-full h-full" xmlns="http://www.w3.org/2000/svg">
             <defs>
@@ -32,12 +17,6 @@ function HomePage() {
               </pattern>
             </defs>
             <rect width="100%" height="100%" fill="url(#grid)" />
-            <path d="M0 40 Q 20 20, 40 40 T 80 40 T 120 40 T 160 40 T 200 40" fill="none" stroke="#10B981" strokeWidth="2" strokeOpacity="0.2">
-              <animate attributeName="d" dur="10s" repeatCount="indefinite" values="
-                M0 40 Q 20 20, 40 40 T 80 40 T 120 40 T 160 40 T 200 40;
-                M0 40 Q 20 60, 40 40 T 80 40 T 120 40 T 160 40 T 200 40;
-                M0 40 Q 20 20, 40 40 T 80 40 T 120 40 T 160 40 T 200 40" />
-            </path>
           </svg>
         </div>
         <div className="flex flex-col min-h-screen bg-[#fcf9ed]">
@@ -48,13 +27,6 @@ function HomePage() {
           <Footer />
         </div>
       </div>
-
-
-
-
-
-
-
     </AuthProvider>
   );
 }
